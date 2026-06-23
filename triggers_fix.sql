15f1687a8be423258e4908ddc76eb10274ebe082
Leôncio <leoncio@example.com>
2026-06-23 15:21:28 -0300

    chore: remove temp files from git, trigger redeploy

--- triggers_fix.sql
+++
@@ -1,5 +0,0 @@
-DELIMITER ;;
-CREATE TRIGGER after_insert_anuncio AFTER INSERT ON anuncio FOR EACH ROW BEGIN UPDATE desconto SET total_registros = total_registros + 1 WHERE hash = NEW.codDesconto; END;;
-
-CREATE TRIGGER after_delete_anuncio AFTER DELETE ON anuncio FOR EACH ROW BEGIN UPDATE desconto SET total_registros = total_registros - 1 WHERE hash = OLD.codDesconto; END;;
-DELIMITER ;
